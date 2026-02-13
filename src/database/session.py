bf910e902b8c72ff5b2ecf0f478ab5f813c02c9d
João Victor <jv191207@gmail.com>
2026-02-13 01:47:57 -0300

    feat: search by id func

--- src/database/session.py
+++ src/database/session.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, create_engine, Session
 from src.settings import Settings
 from src.models import users
 
-# FIXME
+
 def get_session():
     engine = create_engine(url=Settings().DATABASE_URL, echo=True)
 
--- src/database/utils.py
+++ src/database/utils.py
@@ -1,4 +1,9 @@
 from src.schemas.schemas import UserIn
+from fastapi import HTTPException, status
+from src.types import T_Session
+from src.models.users import User
+
+from sqlmodel import select
 
 def clean_user_data(user: UserIn):
     user.username = user.username.lower().replace(' ', '')
@@ -6,3 +11,25 @@ def clean_user_data(user: UserIn):
     user.password = user.password.replace(' ', '')
 
     return user
+
+
+def search_user(id: int, session: T_Session):
+    """_summary_
+
+    Args:
+        id (int): the user id to be searched
+        session (T_Session): a session to interact with database
+
+    Raises:
+        HTTPException: user not found
+
+    Returns:
+        User: user from database
+    """
+    user = session.scalar(select(User).where(User.id == id))
+
+    if not user:
+        raise HTTPException(detail='user not found', status_code=status.HTTP_404_NOT_FOUND)
+
+    return user
+
